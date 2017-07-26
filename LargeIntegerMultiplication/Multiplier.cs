277e3f3fcb9336525ffa3f30566624fe246d4742
Azbus <onix.09@mail.ru>
2017-07-26 19:44:34 +0300

    Добавьте файлы проекта.

---
+++ LargeIntegerMultiplication/Multiplier.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargeIntegerMultiplication
+{
+    class Multiplier
+    {
+        private Multiplier() { }
+
+        // add Furer algorithm
+        public static int[] multiply(int[] a, int[] b)
+        {
+            int[] result;
+
+            int N;
+            if (a.Length < b.Length)
+                N = a.Length;
+            else
+                N = b.Length;
+
+            List<int[]> summands = new List<int[]>();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                int[] tempValue = new int[N];
+                for (int j = 0; j < b.Length; j++)
+                {
+                    tempValue[j] += a[i] * b[j];
+                }
+
+                summands.Add(tempValue);
+            }
+
+            int[] sum = getAmount(summands);
+            result = getResult(sum);
+
+            return result;
+        }
+
+        private static int[] getAmount(List<int[]> summands)
+        {
+            int N = summands.Count + summands.ElementAt(0).Length - 1;
+            int[] sum = new int[N];
+
+            int j = 0;
+            foreach (var summand in summands)
+            {
+                for (int i = 0; i < summand.Length; i++)
+                {
+                        sum[i + j] += summand[i];
+                }
+                j++;
+            }
+
+            return sum;
+        }
+        private static int[] getResult(int[] sum)
+        {
+            int[] result = new int[1];
+
+            return result;
+        }
+    }
+}
---
+++ LargeIntegerMultiplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargeIntegerMultiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 6, 5, 4 };
+            int[] b = { 3, 2, 1 };
+            
+            Multiplier.multiply(a, b);
+        }
+    }
+}
